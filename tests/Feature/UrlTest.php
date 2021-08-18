7d7d9defc55faacbe550d2a925685d6cf7f6d3d4
Samat Tulegenov <magnit56@gmail.com>
2021-08-19 03:28:12 +0500

    Добавлены тесты на urls обработчики

---
+++ tests/Feature/UrlTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Carbon\Carbon;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\DB;
+
+class UrlTest extends TestCase
+{
+    use RefreshDatabase;
+    protected $id = 1;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        DB::table('urls')->truncate();
+        $name = 'https://google.com';
+        DB::table('urls')->insert(
+            [
+                'name' => $name,
+            ]
+        );
+        $this->id = DB::table('urls')
+            ->where('name', $name)
+            ->first()->id;
+    }
+
+    public function testIndex()
+    {
+        $response = $this->get(route('urls.index'));
+        $response->assertOk();
+    }
+
+    public function testCreate()
+    {
+        $response = $this->get(route('urls.create'));
+        $response->assertOk();
+    }
+
+    public function testStore()
+    {
+        $data = ['name' => 'https://ya.ru'];
+        $request = ['url' => $data];
+        $response = $this->post(route('urls.store'), $request);
+        $response->assertSessionHasNoErrors();
+        $response->assertRedirect();
+        $this->assertDatabaseHas('urls', $data);
+    }
+
+    public function testShow()
+    {
+        $response = $this->get(route('urls.show', $this->id));
+        $response->assertOk();
+    }
+}
