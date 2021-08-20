1b400573e581cf469d399a8772f741bb179c2f25
Samat Tulegenov <magnit56@gmail.com>
2021-08-21 04:28:00 +0500

    в шаблон url добавлено отображение url_checks и кнопка запуска проверки

--- resources/views/url.blade.php
+++ resources/views/url.blade.php
@@ -28,6 +28,36 @@
                     </tbody>
                 </table>
             </div>
+            <h2>Проверки</h2>
+            <form action="{{ $id }}/checks" method="POST" class="d-flex justify-content-left">
+                {{ csrf_field() }}
+                <input type="hidden">
+                <button type="submit" class="btn btn-primary">Запустить проверку</button>
+            </form>
+            <div class="table-responsive">
+                <table class="table table-bordered table-hover text-nowrap">
+                    <tbody>
+                    <tr>
+                        <th>ID</th>
+                        {{--                        <th>Код ответа</th>--}}
+                        {{--                        <th>h1</th>--}}
+                        {{--                        <th>keywords</th>--}}
+                        {{--                        <th>description</th>--}}
+                        <th>Дата создания</th>
+                    </tr>
+                    @foreach($checks as $check)
+                        <tr>
+                            <td>{{ $check->id }}</td>
+                            {{--                            <td>{{ $check->status_code }}</td>--}}
+                            {{--                            <td>{{ $check->h1 }}</td>--}}
+                            {{--                            <td>{{ $check->keywords }}</td>--}}
+                            {{--                            <td>{{ $check->description }}</td>--}}
+                            <td>{{ $check->created_at }}</td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
     </main>
 @endsection
