e20a901123ce049effe7df5594872f9c56fe3d8a
Samat Tulegenov <magnit56@gmail.com>
2021-08-26 02:59:15 +0500

    Тест UrlCheck переделан на использование очереди и http::fake

--- tests/Feature/UrlCheckTest.php
+++ tests/Feature/UrlCheckTest.php
@@ -5,32 +5,45 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Queue;
+use App\Jobs\StoreSeoInformation;
 
 class UrlCheckTest extends TestCase
 {
     use RefreshDatabase;
     protected $id;
+    protected $name;
 
     protected function setUp(): void
     {
         parent::setUp();
-        $name = 'https://google.com';
-        DB::table('urls')->insert(
-            [
-                'name' => $name,
-            ]
-        );
-        $this->id = DB::table('urls')
-            ->where('name', $name)
-            ->first()->id;
+        $this->name = 'https://google.com';
+        $data = ['name' => $this->name];
+        $this->id = DB::table('urls')->insertGetId($data);
     }
 
     public function testStore()
     {
+        Queue::fake();
+
         $response = $this->post(route('url_checks.store', ['id' => $this->id]));
         $response->assertSessionHasNoErrors();
         $response->assertRedirect();
 
-        $this->assertDatabaseHas('url_checks', ['id' => 1]);
+        Queue::assertPushed(StoreSeoInformation::class);
+    }
+
+    public function testStoreSeoInformation()
+    {
+        Http::fake([
+            "{$this->name}" => Http::response('Hello World', 200),
+        ]);
+        $data = [
+            'id' => 1,
+            'status_code' => 200,
+        ];
+        StoreSeoInformation::dispatch($this->id, $this->name);
+        $this->assertDatabaseHas('url_checks', $data);
     }
 }
